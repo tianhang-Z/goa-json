5ed977a6763c036318d5421d4c99979c8f36a195
thz <505503675@qq.com>
2024-11-03 22:55:18 +0800

    first

--- build/include/CMakeFiles/goa-json.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "../lib/libgoa-json.a"
-  "../lib/libgoa-json.pdb"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/goa-json.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
---
+++ test/test_fileread.cc
@@ -0,0 +1,66 @@
+#include <gtest/gtest.h>
+#include <iostream>
+
+#include <Writer.hpp>
+#include <StringWriteStream.hpp>
+#include <Document.hpp>
+
+using namespace goa::json;
+
+inline void TEST_FILEREAD(const std::string &json)
+{
+    Document doc;
+    ParseError err = doc.parse(json);
+    EXPECT_EQ(err, ParseError::PARSE_OK);
+    StringWriteStream os;
+    Writer writer(os);
+    doc.writeTo(writer);
+    EXPECT_EQ(json, os.getStringView());
+}
+
+std::string jsonDir("../../bench/taobao/cart.json");
+
+TEST(FileRelative, read)
+{
+    FILE *input = fopen(jsonDir.c_str(), "r");
+    if (input == nullptr)
+        exit(1);
+    FileReadStream is(input);
+    assert(is.peek() == '{' && "wrong peek");
+    fclose(input);
+    EXPECT_TRUE(true);
+}
+
+TEST(FileRelative, read_parse)
+{
+    FILE *input = fopen(jsonDir.c_str(), "r");
+    if (input == nullptr)
+        exit(1);
+    FileReadStream is(input);
+    fclose(input);
+    Document doc;
+    EXPECT_EQ(doc.parseStream(is), ParseError::PARSE_OK);
+}
+
+TEST(FileRelative, read_parse_write)
+{
+    FILE *input = fopen(jsonDir.c_str(), "r");
+    if (input == nullptr)
+        exit(1);
+    FileReadStream is(input);
+    fclose(input);
+    Document doc;
+    doc.parseStream(is);
+
+    StringWriteStream os;
+    Writer writer(os);
+    doc.writeTo(writer);
+    bool b = os.getStringView().size() > 0 && os.getStringView()[0] == '{';
+    EXPECT_TRUE(b);
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
